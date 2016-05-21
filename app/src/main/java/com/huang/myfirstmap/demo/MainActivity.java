d61cb71526dd83e590f541a259b319a7808d0444
huang <527023274@qq.com>
2016-05-21 09:37:12 +0800

    lang 0521

--- app/src/main/java/com/huang/myfirstmap/demo/MainActivity.java
+++ app/src/main/java/com/huang/myfirstmap/demo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.huang.myfirstmap.demo;
 
-import android.app.Fragment;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.widget.RadioGroup;
 import com.huang.myfirstmap.demo.fragment.DestinationFragment;
@@ -15,7 +18,7 @@ import java.util.List;
 
 public class MainActivity extends FragmentActivity {
     private RadioGroup rgs;//用来切换各个页面
-    public List<Fragment> fragments = new ArrayList<Fragment>();//将5个fragment添加到这个list里
+    public List<Fragment> fragments = new ArrayList<Fragment>();//将3个fragment添加到这个list里
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +34,30 @@ public class MainActivity extends FragmentActivity {
         //写的一个fragment的适配器，进行几个页面的逻辑跳转
         new FragmentAdapter(MainActivity.this, fragments, R.id.Fragment, rgs, this);
 
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/huang/myfirstmap/demo/fragment/FragmentAdapter.java
+++ app/src/main/java/com/huang/myfirstmap/demo/fragment/FragmentAdapter.java
@@ -1,12 +1,12 @@
 package com.huang.myfirstmap.demo.fragment;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.support.v4.app.FragmentActivity;
-import android.widget.RadioGroup;
-
 import java.util.List;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.RadioGroup;
 
 /**
  * Created by huang on 2016/5/19.
@@ -31,8 +31,63 @@ public class FragmentAdapter implements RadioGroup.OnCheckedChangeListener {
 
         rGroup.setOnCheckedChangeListener(this);
     }
+
+    /**
+     * 改变Tab键，进行更换页面
+     */
+
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
+        for (int i = 0; i < rGroup.getChildCount(); i++) {
+            if (rGroup.getChildAt(i).getId() == checkedId) {
+                Fragment fragment = fragments.get(i);
+                FragmentTransaction ft = obtainFragmentTransaction(i);
+                getCurrentFragment().onPause();// 暂停当前页面
+                if (fragment.isAdded()) {
+                    fragment.onResume();// 启动目标tab的onResume()
+                } else {
+                    ft.add(fgContentId, fragment);
+                }
+                showFragment(i);// 显示目标tab
+                ft.commit();
+            }
+        }
+    }
 
+    /**
+     * 显示切换的页面
+     * @param i
+     */
+    private void showFragment(int i) {
+        for (int i1 = 0; i1 < fragments.size(); i1++) {
+            Fragment fragment = fragments.get(i1);
+            FragmentTransaction ft = obtainFragmentTransaction(i1);
+            if (i == i1) {
+                ft.show(fragment);
+            } else {
+                ft.hide(fragment);
+            }
+            ft.commit();
+        }
+        currentId = i;// 更新目标tab为当前tab
+    }
+
+    /**
+     * 得到当前的页面
+     * @return
+     */
+    private Fragment getCurrentFragment() {
+        return fragments.get(currentId);
+    }
+
+    /**
+     * 得到当前页面的事务管理
+     * @param i
+     * @return
+     */
+    private FragmentTransaction obtainFragmentTransaction(int i) {
+        FragmentTransaction fg = activity.getSupportFragmentManager()
+                .beginTransaction();
+        return fg;
     }
 }
